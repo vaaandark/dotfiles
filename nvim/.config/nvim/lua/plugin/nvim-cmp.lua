b15a9ab757d07cd9c1d26463cdcf10a4ad782927
Wenda Zhang <vaaandark@gmail.com>
2022-07-27 22:58:49 +0800

    nvim: update nvim-cmp config

--- nvim/.config/nvim/lua/plugin/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugin/nvim-cmp.lua
@@ -33,15 +33,6 @@ cmp.setup({
   })
 })
 
--- Set configuration for specific filetype.
-cmp.setup.filetype('gitcommit', {
-  sources = cmp.config.sources({
-    { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-  }, {
-    { name = 'buffer' },
-  })
-})
-
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline('/', {
   mapping = cmp.mapping.preset.cmdline(),
@@ -60,27 +51,9 @@ cmp.setup.cmdline(':', {
   })
 })
 
--- Setup lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
--- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-require('lspconfig')['clangd'].setup {
-  capabilities = capabilities
-}
-require('lspconfig')['bashls'].setup {
-  capabilities = capabilities
-}
-require('lspconfig')['rust_analyzer'].setup{
-    on_attach = on_attach,
-    flags = lsp_flags,
-    -- Server-specific settings...
-    settings = {
-      ["rust-analyzer"] = {}
-    }
-}
-require('lspconfig')['pyright'].setup{
-  capabilities = capabilities
-}
-require('lspconfig')['sumneko_lua'].setup{
-  capabilities = capabilities
-}
+require('lspconfig')['clangd'].setup {}
+require('lspconfig')['bashls'].setup{}
+require('lspconfig')['rust_analyzer'].setup{}
+require('lspconfig')['pyright'].setup{}
+require('lspconfig')['sumneko_lua'].setup{}
 
