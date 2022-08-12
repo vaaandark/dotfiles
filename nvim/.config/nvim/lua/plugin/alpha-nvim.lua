8756dbbbf1ead3e259ccc0a9d10a009caff15a82
vaaandark <vaaandark@gmail.com>
2022-08-13 02:29:21 +0800

    nvim: edit the config and config dir in vsplit mode

--- nvim/.config/nvim/lua/plugin/alpha-nvim.lua
+++ nvim/.config/nvim/lua/plugin/alpha-nvim.lua
@@ -51,7 +51,7 @@ dashboard.section.buttons.val = {
     dashboard.button( "e", "  New file" , ":ene <BAR> startinsert <CR>"),
     dashboard.button( "f", "  Find file", ":cd $HOME/src | Telescope find_files<CR>"),
     dashboard.button( "r", "  Recent"   , ":Telescope oldfiles<CR>"),
-    dashboard.button( "s", "  Settings" , ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
+    dashboard.button( "s", "  Settings" , ":e $MYVIMRC | :cd %:p:h | vsplit . | wincmd k | pwd<CR>"),
     dashboard.button( "q", "  Quit NVIM", ":qa<CR>"),
 }
 
