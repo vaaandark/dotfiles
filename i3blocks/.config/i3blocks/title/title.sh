b4ebe8cdb5e21b8487ffe9438eaed938b1d00e8a
vaaandark <vaaandark@gmail.com>
2022-08-13 10:53:12 +0800

    i3wm: present the title better

---
+++ i3blocks/.config/i3blocks/title/title.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+title=$(xdotool getwindowname "$(xdotool getwindowfocus -f)")
+main_title="$(echo "$title" | awk -F '-' '{ print ""$NF"" }')"
+#echo "$main_title"
+sub_title="${title%%"$main_title"}"
+if [[ "${#sub_title}" -gt 32 ]]; then
+  sub_title="${sub_title:0:25}... -"
+fi
+echo "$sub_title""$main_title"
