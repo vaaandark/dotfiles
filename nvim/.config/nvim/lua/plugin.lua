fd0a68d0dccddccff731eca80a647ffc7f556333
Wenda Zhang <vaaandark@gmail.com>
2022-08-13 20:23:43 +0800

    nvim: use packer.nvim as plugin manager

--- nvim/.config/nvim/lua/plugin.lua
+++ nvim/.config/nvim/lua/plugin.lua
@@ -1,44 +1,68 @@
-local Plug = vim.fn['plug#']
+--local Plug = vim.fn['plug#']
+--
+--vim.call('plug#begin', '~/.config/nvim/plugged')
+--
+--Plug 'jiangmiao/auto-pairs'
+--Plug 'luochen1990/rainbow'
+--Plug 'iamcco/markdown-preview.nvim'
+--Plug 'skywind3000/vim-terminal-help'
+--Plug 'skywind3000/asyncrun'
+--Plug 'morhetz/gruvbox'
+--Plug 'lambdalisue/suda.vim'
+--Plug 'Yggdroot/indentLine'
+--Plug 'tpope/vim-surround'
+--Plug 'nvim-lualine/lualine.nvim'
+--Plug 'kyazdani42/nvim-web-devicons'
+--Plug 'goolord/alpha-nvim'
+--Plug 'nvim-lua/plenary.nvim'
+--Plug 'nvim-telescope/telescope.nvim'
+--
+--Plug 'neovim/nvim-lspconfig'
+--Plug 'hrsh7th/cmp-nvim-lsp'
+--Plug 'hrsh7th/cmp-buffer'
+--Plug 'hrsh7th/cmp-path'
+--Plug 'hrsh7th/cmp-cmdline'
+--Plug 'hrsh7th/nvim-cmp'
+--
+--Plug 'hrsh7th/cmp-vsnip'
+--Plug 'hrsh7th/vim-vsnip'
+--
+--Plug 'neovim/nvim-lspconfig'
+--
+--vim.call('plug#end')
 
-vim.call('plug#begin', '~/.config/nvim/plugged')
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
-Plug 'jiangmiao/auto-pairs'
-Plug 'luochen1990/rainbow'
-Plug 'iamcco/markdown-preview.nvim'
-Plug 'skywind3000/vim-terminal-help'
-Plug 'skywind3000/asyncrun'
-Plug 'morhetz/gruvbox'
-Plug 'lambdalisue/suda.vim'
-Plug 'Yggdroot/indentLine'
-Plug 'tpope/vim-surround'
-Plug 'nvim-lualine/lualine.nvim'
-Plug 'kyazdani42/nvim-web-devicons'
-Plug 'goolord/alpha-nvim'
-Plug 'nvim-lua/plenary.nvim'
-Plug 'nvim-telescope/telescope.nvim'
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
 
-Plug 'neovim/nvim-lspconfig'
-Plug 'hrsh7th/cmp-nvim-lsp'
-Plug 'hrsh7th/cmp-buffer'
-Plug 'hrsh7th/cmp-path'
-Plug 'hrsh7th/cmp-cmdline'
-Plug 'hrsh7th/nvim-cmp'
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
 
-Plug 'hrsh7th/cmp-vsnip'
-Plug 'hrsh7th/vim-vsnip'
+  use 'jiangmiao/auto-pairs'
+  use 'luochen1990/rainbow'
+  use 'iamcco/markdown-preview.nvim'
+  use 'skywind3000/vim-terminal-help'
+  use 'skywind3000/asyncrun.vim'
+  use 'morhetz/gruvbox'
+  use 'lambdalisue/suda.vim'
+  use 'Yggdroot/indentLine'
+  use 'tpope/vim-surround'
+  use 'nvim-lualine/lualine.nvim'
+  use 'kyazdani42/nvim-web-devicons'
+  use 'goolord/alpha-nvim'
+  use 'nvim-lua/plenary.nvim'
+  use 'nvim-telescope/telescope.nvim'
 
-Plug 'neovim/nvim-lspconfig'
+  use 'neovim/nvim-lspconfig'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/nvim-cmp'
 
-vim.call('plug#end')
+  use 'hrsh7th/cmp-vsnip'
+  use 'hrsh7th/vim-vsnip'
 
-require('plugin/rainbow')
-require('plugin/markdown-preview')
-require('plugin/asyncrun')
-require('plugin/suda')
-require('plugin/alpha-nvim')
-require('plugin/telescope')
-require('plugin/vim-vsnip')
-require('plugin/nvim-cmp')
-require('plugin/lualine')
-
-vim.cmd([[colorscheme gruvbox]])
+end)
