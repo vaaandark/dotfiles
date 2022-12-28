f6b3ff810cd567052d6dfbe817f3c4b3fd8fb800
vaaandark <vaaandark@gmail.com>
2022-12-29 02:05:33 +0800

    nvim: make lua coding style better

--- nvim/.config/nvim/lua/autocmd.lua
+++ nvim/.config/nvim/lua/autocmd.lua
@@ -1,20 +1,21 @@
 -- auto commands
 
-vim.cmd('autocmd FileType c set tabstop=4 | set shiftwidth=4')
-vim.cmd('autocmd FileType arduino set tabstop=2 | set shiftwidth=2')
-vim.cmd('autocmd FileType cpp set tabstop=2 | set shiftwidth=2')
-vim.cmd('autocmd FileType python set tabstop=2 | set shiftwidth=2')
-vim.cmd('autocmd FileType asm set tabstop=2 | set shiftwidth=2')
-vim.cmd('autocmd FileType sh set tabstop=2 | set shiftwidth=2')
-vim.cmd('autocmd FileType zsh set tabstop=2 | set shiftwidth=2')
-vim.cmd('autocmd FileType lisp set tabstop=2 | set shiftwidth=2')
-vim.cmd('autocmd FileType lua set tabstop=2 | set shiftwidth=2')
-
 vim.cmd([[
+autocmd FileType c set tabstop=4 | set shiftwidth=4
+autocmd FileType arduino set tabstop=2 | set shiftwidth=2
+autocmd FileType cpp set tabstop=2 | set shiftwidth=2
+autocmd FileType python set tabstop=2 | set shiftwidth=2
+autocmd FileType asm set tabstop=2 | set shiftwidth=2
+autocmd FileType sh set tabstop=2 | set shiftwidth=2
+autocmd FileType zsh set tabstop=2 | set shiftwidth=2
+autocmd FileType lisp set tabstop=2 | set shiftwidth=2
+autocmd FileType lua set tabstop=2 | set shiftwidth=2
+
 augroup filetypedetect
   au! BufRead,BufNewFile *.h         setfiletype c
 augroup END
-]])
-vim.cmd([[
+
 autocmd BufReadPost * if line("'\"'") > 1 && line("'\'") <= line("$") | exe "normal! g'\"" | endif
+
+au TextYankPost * silent! lua vim.highlight.on_yank{higroup="IncSearch", timeout=700}
 ]])
