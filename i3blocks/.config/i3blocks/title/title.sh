e7407b89c6040cc8b2a2217da6d77c4db74e61c5
Wenda Zhang <vaaandark@gmail.com>
2022-12-29 02:03:11 +0800

    i3blocks: update title.sh, when main title is too long, it will be omited

--- i3blocks/.config/i3blocks/title/title.sh
+++ i3blocks/.config/i3blocks/title/title.sh
@@ -8,4 +8,8 @@ sub_title="${title%%"$main_title"}"
 if [[ "${#sub_title}" -gt 25 ]]; then
   sub_title="${sub_title::20}... -"
 fi
+main_len="${#main_title}"
+if [[ main_len -gt 25 ]]; then
+  main_title="... ${main_title:main_len-20:20}"
+fi
 echo "$sub_title""$main_title" | tr '[:lower:]' '[:upper:]'
