089025c35132b758687e6d050675d6bfd51b6ab3
Wenda Zhang <vaaandark@gmail.com>
2023-02-09 12:12:23 +0800

    nvim: use api `vim.api.nvim_command` to simplify codes

--- nvim/.config/nvim/lua/run.lua
+++ nvim/.config/nvim/lua/run.lua
@@ -1,30 +1,18 @@
 function RunCode()
   filetype = vim.o.filetype
   if (filetype == 'c') then
-    vim.cmd(':AsyncRun name=$(basename % .c); clang -std=c99 -Wall -Wshadow -g -fsanitize=address -O0 % -o /tmp/"$name"-$$ || exit 1; /tmp/"$name"-$$ "$@"; ret=$?; rm /tmp/"$name"-$$; exit $ret')
+    vim.api.nvim_command('AsyncRun name=$(basename % .c); clang -std=c99 -Wall -Wshadow -g -fsanitize=address -O0 % -o /tmp/"$name"-$$ || exit 1; /tmp/"$name"-$$ "$@"; ret=$?; rm /tmp/"$name"-$$; exit $ret')
   elseif (filetype == 'cpp') then
-    vim.cmd([[
-      :AsyncRun name=$(basename % .cpp); clang++ -std=c++2a -Wall -Wshadow -g -fsanitize=address -O0 % -o /tmp/"$name"-$$ || exit 1; /tmp/"$name"-$$ "$@"; ret=$?; rm /tmp/"$name"-$$; exit $ret
-    ]])
+    vim.api.nvim_command('AsyncRun name=$(basename % .cpp); clang++ -std=c++2a -Wall -Wshadow -g -fsanitize=address -O0 % -o /tmp/"$name"-$$ || exit 1; /tmp/"$name"-$$ "$@"; ret=$?; rm /tmp/"$name"-$$; exit $ret')
   elseif (filetype == 'asm') then
-    vim.cmd([[
-      :AsyncRun name=$(basename % .asm); as --32 % -o /tmp/"$name"-$$.o || exit 1; ld -m elf_i386 /tmp/"$name"-$$.o -o /tmp/"$name"-$$ || exit 1; /tmp/"$name"-$$ "$@"; ret=$?; rm /tmp/"$name"-$$ /tmp/"$name"-$$.o; exit $ret
-    ]])
+    vim.api.nvim_command('AsyncRun name=$(basename % .asm); as --32 % -o /tmp/"$name"-$$.o || exit 1; ld -m elf_i386 /tmp/"$name"-$$.o -o /tmp/"$name"-$$ || exit 1; /tmp/"$name"-$$ "$@"; ret=$?; rm /tmp/"$name"-$$ /tmp/"$name"-$$.o; exit $ret')
   elseif (filetype == 'rust') then
-    vim.cmd([[
-      :AsyncRun name=$(basename % .rs); rustc % -o /tmp/"$name"-$$ || exit 1; /tmp/"$name"-$$ "$@"; ret=$?; rm /tmp/"$name"-$$; exit $ret
-    ]])
+    vim.api.nvim_command('AsyncRun name=$(basename % .rs); rustc % -o /tmp/"$name"-$$ || exit 1; /tmp/"$name"-$$ "$@"; ret=$?; rm /tmp/"$name"-$$; exit $ret')
   elseif (filetype == 'lua') then
-    vim.cmd([[
-      :AsyncRun lua %
-    ]])
+    vim.api.nvim_command('AsyncRun lua %')
   elseif (filetype == 'lisp') then
-    vim.cmd([[
-      :AsyncRun clisp %
-    ]])
+    vim.api.nvim_command(':AsyncRun clisp %')
   elseif (filetype == 'python') then
-    vim.cmd([[
-      :AsyncRun python %
-    ]])
+    vim.api.nvim_command(':AsyncRun python %')
   end
 end
